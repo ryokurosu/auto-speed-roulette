99dcf04fb424a97fbb88a8bc3ae0768e20fc87c2
Ryo Kurosu <knowrop1208@gmail.com>
2019-10-27 14:15:26 +0900

    fix: in command

--- cron.py
+++ cron.py
@@ -24,14 +24,14 @@ from logging import getLogger, StreamHandler, DEBUG, FileHandler, Formatter
 nowdate = datetime.datetime.today().strftime("%Y%m%d_%H%M%S")
 logger = getLogger(__name__)
 handler = StreamHandler()
-handler = FileHandler(filename="./logs/" + nowdate + ".log")
+handler = FileHandler(filename="./logs/" + nowdate + ".log", encoding="utf-8")
 handler.setLevel(DEBUG)
 handler.setFormatter(Formatter("-----------------------\n%(asctime)s %(levelname)8s %(message)s"))
 logger.setLevel(DEBUG)
 logger.addHandler(handler)
 logger.propagate = False
 
-version = "1.6.15"
+version = "1.7.0"
 
 filter_time = 65;
 filter_time_after = 88;
@@ -39,8 +39,14 @@ filter_count_under = 4;
 filter_odds = 1.05;
 filter_count = 5;
 
+browser = ""
+start_time = datetime.datetime.now()
+notified = []
+loopcount = 0
 
-def logger_set(logger):
+
+def logger_set():
+	global logger
 	for h in logger.handlers:
 		logger.removeHandler(h)
 	nowdate = datetime.datetime.today().strftime("%Y%m%d_%H%M%S")
@@ -128,192 +134,112 @@ def check_notified(a_team, b_team, notified):
 			pass
 	return False
 
-now = datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S")
-message_text = "Time : " + now + " 起動しました SLB Ver." + version
-logger.debug(message_text)
-message.send_debug_message(message_text)
+def start_browser():
+	global browser
+	global start_time
+	firstURL = "https://mobile.bet365.com/"
+	startURL = "https://mobile.bet365.com/?nr=1#/IP/"
+	if browser != "":
+		browser.quit()
+	now = datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S")
+	message_text = "Time : " + now + " 起動しました SLB Ver." + version
+	start_time = datetime.datetime.now()
+	logger.debug(message_text)
+	message.send_debug_message(message_text)
+	uas = ["Mozilla/5.0 (iPad; CPU OS 11_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/11.0 Mobile/15A5341f Safari/604.1",
+	"Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1",
+	"Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Mobile Safari/537.36",
+	"Mozilla/5.0 (Linux; U; Android 4.3; en-us; SM-N900T Build/JSS15J) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
+	"Mozilla/5.0 (Linux; Android 8.0; Pixel 2 Build/OPD3.170816.012) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Mobile Safari/537.36"]
+	# random.shuffle(uas)
+	base = os.path.dirname(os.path.abspath(__file__))
+	options = webdriver.ChromeOptions()
+	mobile_emulation = {
+	    "deviceMetrics": { "width": 1024, "height": 1366, "pixelRatio": 3.0 },
+	    "userAgent": uas[0] }
+	options.add_argument('--no-sandbox')
+	options.add_argument('--lang=ja-JP')
+	options.add_argument("--incognito")
+	options.add_experimental_option("mobileEmulation", mobile_emulation)
+	if os.name == 'nt':
+		browser = webdriver.Chrome(os.path.normpath(os.path.join(base, "./chromedriver.exe")),options=options)
+	else:
+		browser = webdriver.Chrome(os.path.normpath(os.path.join(base, "./chromedriver")),options=options)
+	browser.get("https://www.google.com/?hl=ja")
+	browser.implicitly_wait(3)
+	browser.get(firstURL)
+	time.sleep(1)
+	browser.get(startURL)
+	logger_set()
+	soccer_click()
 
 
-uas = ["Mozilla/5.0 (iPad; CPU OS 11_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/11.0 Mobile/15A5341f Safari/604.1",
-"Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1",
-"Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Mobile Safari/537.36",
-"Mozilla/5.0 (Linux; U; Android 4.3; en-us; SM-N900T Build/JSS15J) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
-"Mozilla/5.0 (Linux; Android 8.0; Pixel 2 Build/OPD3.170816.012) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Mobile Safari/537.36"]
-# random.shuffle(uas)
+def soccer_click():
+	check = False
+	for b in browser.find_elements_by_css_selector('.hm-TabletNavButtons_Link'):
+		if "In-Play" in b.text:
+			b.click()
 
-base = os.path.dirname(os.path.abspath(__file__))
-options = webdriver.ChromeOptions()
-mobile_emulation = {
-    "deviceMetrics": { "width": 1024, "height": 1366, "pixelRatio": 3.0 },
-    "userAgent": uas[0] }
-options.add_argument('--no-sandbox')
-options.add_argument('--lang=ja-JP')
-options.add_argument("--incognito")
-options.add_experimental_option("mobileEmulation", mobile_emulation)
-# options.add_experimental_option("excludeSwitches", ["enable-automation"])
-# options.add_experimental_option('useAutomationExtension', False)
+	stop_count = 0
+	while(not check):
+		logger.debug('Searching Soccer...')
+		buttons = browser.find_elements_by_css_selector('.ipo-ClassificationMenuBase .ipo-Classification')
+		stop_count = stop_count + 1
+		if stop_count > 50:
+			check = True
+			break
 
-#Webdriver
-# if os.name == 'nt':
-# 	browser = webdriver.Chrome(os.path.normpath(os.path.join(base, "./chromedriver.exe")),options=options)
-# else:
-# 	browser = webdriver.Chrome(os.path.normpath(os.path.join(base, "./chromedriver")),options=options)
+		for b in buttons:
+			try:
+				classname = b.text
+				if 'Soccer' in classname:
+					b.click()
+					logger.debug('go Soccer Page')
+					check = True
+					break
+			except Exception as e:
+				print(traceback.format_exc())
+				pass
+			else:
+				pass
+			finally:
+				pass
 
-if os.name == 'nt':
-	browser = webdriver.Chrome(os.path.normpath(os.path.join(base, "./chromedriver.exe")),options=options)
-else:
-	browser = webdriver.Chrome(os.path.normpath(os.path.join(base, "./chromedriver")),options=options)
+def clear_global_key():
+	global rows
+	rows = "cleared"
+	global row
+	row = "cleared"
+	global buttons
+	buttons = "cleared"
+	global b
+	b = "cleared"
 
-browser.get("https://www.google.com/?hl=ja")
-time.sleep(1)
-firstURL = "https://mobile.bet365.com/"
-startURL = "https://mobile.bet365.com/?nr=1#/IP/"
-browser.implicitly_wait(3)
-browser.get(firstURL)
-time.sleep(1)
-browser.get(startURL)
-logger.debug(message_text)
-time.sleep(1)
-notified = []
-
-
-# alinks = browser.find_elements_by_css_selector('ul.lpnm a.lpdgl')
-# for a in alinks:
-# 	if a.text == "English":
-# 		a.click
-# 		print('click English')
-# 		break
-# matchgoal = False
-
-# try:
-# 	while (not matchgoal):
-# 		browser.find_element_by_css_selector('.ipo-MarketSwitcher').click()
-# 		time.sleep(1)
-# 		divs = browser.find_elements_by_css_selector('.ipo-MarketSwitcherRow')
-# 		for d in divs:
-# 			if "Match Goals" == d.find_element_by_css_selector('.ipo-MarketSwitcherRow_Label').text:
-# 				d.click()
-# 				matchgoal = True
-# 				break
-# except Exception as e:
-# 	message_text = "エラーで停止します。"
-# 	message.send_debug_message(message_text)
-# 	logger.debug(message_text)
-# 	time.sleep(1)
-# 	# os.system("source ~/.bash_profile && sh /home/root/app/cron.sh")
-# 	browser.quit()
-# 	sys.exit()
-# else:
-# 	pass
-# finally:
-# 	pass
-
-
-loopcount = 0
-
-buttons = browser.find_elements_by_css_selector('.ipo-Classification')
-# Soccer Click
-check = False
-for b in buttons:
-	classname = b.find_element_by_css_selector('.ipo-Classification_Name').text
-	if 'Soccer' in classname:
-		b.click
-		logger.debug('go Soccer Page')
-		check = True
-		break
-
-# Exit when soccer is not found
-if not check:
-	print('Soccer is not found.')
-	browser.quit()
-	sys.exit()
-
+start_browser()
 
 row_index = 0
 loop_stop_count = 0
+
 while(True):
 	loopcount = loopcount + 1
 	logger.debug("Loop Count : " + str(loopcount))
-	if loopcount % 30000 == 0:
-		
-		browser.get(startURL)
 
-		for b in browser.find_elements_by_css_selector('.hm-TabletNavButtons_Link'):
-			if "In-Play" in b.text:
-				b.click()
-
-		# browser.get(startURL)
-		
-		now = datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S")
-		message_text = "Time : " + now + " 正常に稼働中..."
+	if start_time <= (datetime.datetime.now() - datetime.timedelta(hours=12)):
+		start_browser()
+		message_text = "メモリ解放のための再起動\n" + datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S")
 		logger.debug(message_text)
 		message.send_debug_message(message_text)
-		logger = logger_set(logger)
-		check = False
-		time.sleep(1)
 
-		stop_count = 0
-		while(not check):
-			logger.debug('Searching Soccer...')
-			buttons = browser.find_elements_by_css_selector('.ipo-ClassificationMenuBase .ipo-Classification')
-			stop_count = stop_count + 1
-			if stop_count > 50:
-				check = True
-				break
-
-			for b in buttons:
-				try:
-					classname = b.text
-					if 'Soccer' in classname:
-						b.click()
-						logger.debug('go Soccer Page')
-						check = True
-						break
-				except Exception as e:
-					print(traceback.format_exc())
-					pass
-				else:
-					pass
-				finally:
-					pass
+	if loopcount % 30000 == 0:
+		message_text = "正常に稼働中...\n" + datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S")
+		logger.debug(message_text)
+		message.send_debug_message(message_text)
+		logger_set()
 				
 	elif loopcount % 10000 == 0 or loop_stop_count > 30:
 		print(loop_stop_count)
-		browser.get(startURL)
-		logger = logger_set(logger)
-		
-		for b in browser.find_elements_by_css_selector('.hm-TabletNavButtons_Link'):
-			if "In-Play" in b.text:
-				b.click()
-
-		check = False
-		time.sleep(1)
-
-		stop_count = 0
-		while(not check):
-			logger.debug('Searching Soccer...')
-			buttons = browser.find_elements_by_css_selector('.ipo-ClassificationMenuBase .ipo-Classification')
-			stop_count = stop_count + 1
-			if stop_count > 50:
-				check = True
-				break
-
-			for b in buttons:
-				try:
-					classname = b.text
-					if 'Soccer' in classname:
-						b.click()
-						logger.debug('go Soccer Page')
-						check = True
-						break
-				except Exception as e:
-					print(traceback.format_exc())
-					pass
-				else:
-					pass
-				finally:
-					pass
-		pass
+		logger_set()
+		soccer_click()
 
 	browser.implicitly_wait(3)
 	skip_count = 0
@@ -384,7 +310,6 @@ while(True):
 
 							if easy_check(play_timer,a_team,b_team,under,odds) and check_rules(play_timer, a_team, b_team, a_team_count, b_team_count, under, odds) and not check_notified(a_team,b_team,notified):
 								message.send_debug_message("HIT!")
-								now = datetime.datetime.today().strftime("%Y/%m/%d %H:%M:%S")
 								googleurl = "https://www.google.com/search?q=" + urllib.parse.quote(a_team + " VS " + b_team)
 								message_text = "／\nSLB配信 ベット通知\n＼\n\n"\
 												"【種目】サッカー\n" + a_team + " VS " + b_team +  "\n"\
@@ -393,7 +318,7 @@ while(True):
 							                    "【現在のオッズ】" + str(odds) + "\n\n"\
 							                    "下のURLから直接ベットしてください。↓↓\n"\
 							                    "【ベットURL】\n" + browser.current_url + "\n"\
-							                    "\n※時間経過により、オッズが微妙に変動している可能性があります。\n\nPowered by SLB.\n" + now + "\n\n試合結果URL：" + googleurl
+							                    "\n※時間経過により、オッズが微妙に変動している可能性があります。\n\nPowered by SLB.\n" + datetime.datetime.today().strftime("%Y/%m/%d %H:%M:%S") + "\n\n試合結果URL：" + googleurl
 								message.send_all_message(message_text)
 								message.send_debug_message(message_text)
 								logger.debug('send Line Message')
@@ -424,7 +349,7 @@ while(True):
 		pass
 	finally:
 		row_index = row_index + 1
-		del rows
+		clear_global_key()
 		pass
 	continue
 
